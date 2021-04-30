e87693d69bff302829b01357b9e6de5f67b891d1
Rodrigo Fraga <digo_via@hotmail.com>
2021-04-29 22:18:44 -0300

    Correção de importação de módulos

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,3 @@
-import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
 import Header from './header'
@@ -14,7 +13,6 @@ const Layout = ({ preview, children }: Props) => {
       <Header/>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   theme: {
